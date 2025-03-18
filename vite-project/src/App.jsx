c85a11e7b4c17f6afba5aee7458488092043a010
John Doe <libbypieper@gmail.com>
2025-03-18 18:44:27 -0500

    block 26

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,23 @@
 import './App.css'
+import { useState } from 'react';
 import ContactList from './components/ContactList';
+import SelectedContact from "./components/SelectedContact";
+
+const App = ()=> {
+  const [contacts, setContacts] = useState(dummyContacts)
+  console.log("Contacts: ", contacts)
+}
+
 export default function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
+
   return (
     <>
+    {selectedContactId ? (
+        <div>Selected Contact View</div>
+      ) : (
       <ContactList />
+      )}
     </>
   );
 }
--- vite-project/src/components/ContactList.jsx
+++ vite-project/src/components/ContactList.jsx
@@ -1,6 +1,33 @@
+import {useEffect} from 'react';
 import React from "react";
+import { useState } from "react";
+import ContactRow from './ContactRow';
 
-export default function ContactList() {
+const dummyContacts = [
+  { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
+  { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
+  { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
+];
+
+useEffect(()=>{
+  async function fetchContacts() {
+    try {
+      const response = await fetch(
+      "https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users"
+    );
+    const result = await response.json();
+    setContacts(result);
+      // your fetch logic will go here
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  fetchContacts()
+},[])
+
+ContactList setSelectedContactId={setSelectedContactId} 
+
+export default function ContactList({ setSelectedContactId }) {
 
   return (
     <table>
@@ -15,8 +42,9 @@ export default function ContactList() {
         <td>Email</td>
         <td>Phone</td>
       </tr>
-      {
-         // Map over data here
+      {contacts.map((contact) => {
+          return <ContactRow key={contact.id} contact={contact} />;
+        })  // Map over data here
        }
     </tbody>
   </table>
